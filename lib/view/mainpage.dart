69edd89d03274391748f046118bef69ca8cb82d5
Yaşar BAYKUŞ <byyasar@gmail.com>
2022-01-14 20:44:07 +0300

    ana ekran seçimler oluşturuldu.

--- lib/view/mainpage.dart
+++ lib/view/mainpage.dart
@@ -5,6 +5,7 @@ import 'package:flutter_application_hive/core/boxes.dart';
 import 'package:flutter_application_hive/core/widget/custom_ders_dialog.dart';
 import 'package:flutter_application_hive/core/widget/custom_menu_button.dart';
 import 'package:flutter_application_hive/core/widget/custom_sinif_dialog.dart';
+import 'package:flutter_application_hive/core/widget/custom_temrin_dialog.dart';
 import 'package:flutter_application_hive/features/dersler/model/ders_model.dart';
 import 'package:flutter_application_hive/features/dersler/store/ders_store.dart';
 import 'package:flutter_application_hive/features/dersler/view/ders_view.dart';
@@ -16,6 +17,8 @@ import 'package:flutter_application_hive/features/ogrenci/view/ogrenci_view.dart
 import 'package:flutter_application_hive/features/siniflar/model/sinif_model.dart';
 import 'package:flutter_application_hive/features/siniflar/store/sinif_store.dart';
 import 'package:flutter_application_hive/features/siniflar/view/sinif_view.dart';
+import 'package:flutter_application_hive/features/temrin/model/temrin_model.dart';
+import 'package:flutter_application_hive/features/temrin/store/temrin_store.dart';
 import 'package:flutter_application_hive/features/temrin/view/temrin_view.dart';
 import 'package:flutter_application_hive/features/temrinnot/view/temrinnot_view.dart';
 import 'package:flutter_mobx/flutter_mobx.dart';
@@ -31,17 +34,19 @@ class MainPage extends StatefulWidget {
 }
 
 class _MainPageState extends BaseState<MainPage> {
-  List<SinifModel> transactionsSinif = [];
-  List<DersModel> transactionsDers = [];
+  /* List<SinifModel> _transactionsSinif = [];
+  List<DersModel> _transactionsDers = [];
+  List<TemrinModel> _transactionsTemrin = []; */
 
-  SinifStore viewModelSinif = SinifStore();
-  DersStore viewModelDers = DersStore();
+  final _viewModelSinif = SinifStore();
+  final _viewModelDers = DersStore();
+  final _viewModelTemrin = TemrinStore();
 
-  String sinifSecText = "Sınıf Seç";
-  String dersSecText = "Ders Seç";
+  String _sinifSecText = "Sınıf Seç";
+  String _dersSecText = "Ders Seç";
+  String _temrinSecText = "Temrin Seç";
 
-  final DersListesiHelper _dersListesiHelper =
-      DersListesiHelper(ApplicationConstants.boxDers);
+  //final DersListesiHelper _dersListesiHelper = DersListesiHelper(ApplicationConstants.boxDers);
 
   @override
   Widget build(BuildContext context) {
@@ -50,14 +55,22 @@ class _MainPageState extends BaseState<MainPage> {
         floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
         floatingActionButton: _buildFloatingActionButton(context),
         appBar: AppBar(title: const Text('Temrin Not Sistemi')),
-        body: Center(
-          child: Column(
-            children: [
-              const Text('Sınıf:', style: TextStyle(fontSize: 18)),
-              _buildSinifSec(context),
-              const Text('Ders:', style: TextStyle(fontSize: 18)),
-              _buildDersSec(context),
-            ],
+        body: Container(
+          color: Colors.amberAccent,
+          child: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                const Text('Sınıf:', style: TextStyle(fontSize: 18)),
+                _buildSinifSec(context),
+                const Text('Ders:', style: TextStyle(fontSize: 18)),
+                _buildDersSec(context),
+                const Text('Temrin:', style: TextStyle(fontSize: 18)),
+                _buildTemrinSec(context),
+                const SizedBox(height: 20),
+                myCustomMenuButton(context, () {}, const Text('Not Gir'), const Icon(Icons.point_of_sale)),
+              ],
+            ),
           ),
         ),
         drawer: _buildDrawer(context),
@@ -85,8 +98,7 @@ class _MainPageState extends BaseState<MainPage> {
               style: TextStyle(fontSize: 24),
             ),
             onTap: () {
-              Navigator.of(context).push(MaterialPageRoute(
-                  builder: (context) => const SinifpageView()));
+              Navigator.of(context).push(MaterialPageRoute(builder: (context) => const SinifpageView()));
             },
           ),
           const Divider(color: Colors.black, height: 2.0),
@@ -96,8 +108,7 @@ class _MainPageState extends BaseState<MainPage> {
               style: TextStyle(fontSize: 24),
             ),
             onTap: () {
-              Navigator.of(context).push(MaterialPageRoute(
-                  builder: (context) => const DerspageView()));
+              Navigator.of(context).push(MaterialPageRoute(builder: (context) => const DerspageView()));
             },
           ),
           const Divider(color: Colors.black, height: 2.0),
@@ -107,8 +118,7 @@ class _MainPageState extends BaseState<MainPage> {
               style: TextStyle(fontSize: 24),
             ),
             onTap: () {
-              Navigator.of(context).push(MaterialPageRoute(
-                  builder: (context) => const OgrencipageView()));
+              Navigator.of(context).push(MaterialPageRoute(builder: (context) => const OgrencipageView()));
             },
           ),
           const Divider(color: Colors.black, height: 2.0),
@@ -118,8 +128,7 @@ class _MainPageState extends BaseState<MainPage> {
               style: TextStyle(fontSize: 24),
             ),
             onTap: () {
-              Navigator.of(context).push(MaterialPageRoute(
-                  builder: (context) => const TemrinpageView()));
+              Navigator.of(context).push(MaterialPageRoute(builder: (context) => const TemrinpageView()));
             },
           ),
           const Divider(color: Colors.black, height: 2.0),
@@ -129,8 +138,7 @@ class _MainPageState extends BaseState<MainPage> {
               style: TextStyle(fontSize: 24),
             ),
             onTap: () {
-              Navigator.of(context).push(MaterialPageRoute(
-                  builder: (context) => const TemrinnotpageView()));
+              Navigator.of(context).push(MaterialPageRoute(builder: (context) => const TemrinnotpageView()));
             },
           ),
         ],
@@ -144,34 +152,27 @@ class _MainPageState extends BaseState<MainPage> {
       child: Row(
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
-          FloatingActionButton.extended(
-              onPressed: () {}, label: const Text('Temrin Not Gir')),
+          FloatingActionButton.extended(onPressed: () {}, label: const Text('Temrin Not Gir')),
           FloatingActionButton(
               heroTag: 'temrin',
-              shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(8)),
+              shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(8)),
               child: const Icon(Icons.note),
               onPressed: () {
-                Navigator.of(context).push(MaterialPageRoute(
-                    builder: (context) => const TemrinnotpageView()));
+                Navigator.of(context).push(MaterialPageRoute(builder: (context) => const TemrinnotpageView()));
               }),
           FloatingActionButton(
               heroTag: 'data',
-              shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(8)),
+              shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(8)),
               child: const Icon(Icons.data_saver_off),
               onPressed: () async {
-                var raw =
-                    await http.get(Uri.parse(ApplicationConstants.sinifUrl));
+                var raw = await http.get(Uri.parse(ApplicationConstants.sinifUrl));
                 if (raw.statusCode == 200) {
                   var jsonFeedback = convert.jsonDecode(raw.body);
                   //Logger().i('this is json Feedback ${jsonFeedback}');
-                  SinifListesiHelper sinifListesiHelper =
-                      SinifListesiHelper(ApplicationConstants.boxSinif);
+                  SinifListesiHelper sinifListesiHelper = SinifListesiHelper(ApplicationConstants.boxSinif);
 
                   for (var sinif in jsonFeedback) {
-                    sinifListesiHelper.addItem(
-                        SinifModel(id: sinif['id'], sinifAd: sinif['sinifAd']));
+                    sinifListesiHelper.addItem(SinifModel(id: sinif['id'], sinifAd: sinif['sinifAd']));
                   }
                 } else if (raw.statusCode == 404) {
                   //print('sayfa bulunamadı');
@@ -179,23 +180,18 @@ class _MainPageState extends BaseState<MainPage> {
               }),
           FloatingActionButton(
               heroTag: 'dataders',
-              shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(8)),
+              shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(8)),
               child: const Icon(Icons.data_saver_on),
               onPressed: () async {
-                var raw =
-                    await http.get(Uri.parse(ApplicationConstants.dersUrl));
+                var raw = await http.get(Uri.parse(ApplicationConstants.dersUrl));
                 if (raw.statusCode == 200) {
                   var jsonFeedback = convert.jsonDecode(raw.body);
                   Logger().i('this is json Feedback $jsonFeedback');
-                  DersListesiHelper dersListesiHelper =
-                      DersListesiHelper(ApplicationConstants.boxDers);
+                  DersListesiHelper dersListesiHelper = DersListesiHelper(ApplicationConstants.boxDers);
 
                   for (var ders in jsonFeedback) {
-                    dersListesiHelper.addItem(DersModel(
-                        id: ders['id'],
-                        sinifId: ders['sinifId'],
-                        dersad: ders['dersAd']));
+                    dersListesiHelper
+                        .addItem(DersModel(id: ders['id'], sinifId: ders['sinifId'], dersad: ders['dersAd']));
                   }
                 } else if (raw.statusCode == 404) {
                   Logger().e('sayfa bulunamadı');
@@ -203,17 +199,14 @@ class _MainPageState extends BaseState<MainPage> {
               }),
           FloatingActionButton(
               heroTag: 'dataOgrenci',
-              shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(8)),
+              shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(8)),
               child: const Icon(Icons.star_rounded),
               onPressed: () async {
-                var raw = await http
-                    .get(Uri.parse(ApplicationConstants.ogrencilerUrl));
+                var raw = await http.get(Uri.parse(ApplicationConstants.ogrencilerUrl));
                 if (raw.statusCode == 200) {
                   var jsonFeedback = convert.jsonDecode(raw.body);
                   Logger().i('this is json Feedback $jsonFeedback');
-                  OgrenciListesiHelper ogrenciListesiHelper =
-                      OgrenciListesiHelper(ApplicationConstants.boxOgrenci);
+                  OgrenciListesiHelper ogrenciListesiHelper = OgrenciListesiHelper(ApplicationConstants.boxOgrenci);
 
                   for (var ogrenci in jsonFeedback) {
                     ogrenciListesiHelper.addItem(OgrenciModel(
@@ -238,64 +231,85 @@ class _MainPageState extends BaseState<MainPage> {
   }
 
   _addTransactionDers(int id, String dersad, int sinifId) async {
-    final dersModel = DersModel(
-        id: id, dersad: dersad, sinifId: viewModelSinif.filtreSinifId);
+    final dersModel = DersModel(id: id, dersad: dersad, sinifId: _viewModelSinif.filtreSinifId);
     final box = DersBoxes.getTransactions();
     box.add(dersModel);
   }
 
-  List<DersModel>? _getDersItemId(int id) {
-    List<DersModel> transactionsDersListesiGecici =
-        DersBoxes.getTransactions().values.toList().cast<DersModel>();
-    List<DersModel> transactionsDersListesiGeciciFiltre = [];
-
-    for (var ders in transactionsDersListesiGecici) {
-      if (ders.id == id) {
-        transactionsDersListesiGeciciFiltre.add(ders);
-      }
-    }
-    return transactionsDersListesiGeciciFiltre;
+  _addTransactionTemrin(int id, String temrinKonusu, int dersId) async {
+    final transaction = TemrinModel(id: id, temrinKonusu: temrinKonusu, dersId: dersId);
+    final box = TemrinBoxes.getTransactions();
+    box.add(transaction);
   }
 
   _buildSinifSec(BuildContext context) => myCustomMenuButton(context, () {
-        showDialog(
-                context: context,
-                builder: (context) =>
-                    CustomSinifDialog(onClickedDone: _addTransactionSinif))
+        showDialog(context: context, builder: (context) => CustomSinifDialog(onClickedDone: _addTransactionSinif))
             .then((value) {
-          transactionsDers = [];
+          _tumSecimleriSifirla();
           if (value != null) {
-            sinifSecText = value.sinifAd;
-            viewModelSinif.setFiltreSinifId(value.sinifId);
-
+            _sinifSecText = value.sinifAd;
+            _viewModelSinif.setFiltreSinifId(value.sinifId);
             if (value.sinifId == -1) {
-              sinifSecText = "Sınıf Seç";
-            } else {
-              transactionsDers = _getDersItemId(viewModelSinif.filtreSinifId)!;
+              _sinifSecText = "Sınıf Seç";
             }
-            viewModelSinif.setSinifAd(sinifSecText);
-            Logger()
-                .i('Seçilen sınıf id ${value.sinifId} sınıf ${value.sinifAd}');
+            _viewModelSinif.setSinifAd(_sinifSecText);
+            Logger().i('Seçilen sınıf id ${value.sinifId} sınıf ${value.sinifAd}');
           }
         });
-      }, Observer(builder: (context) => Text(viewModelSinif.sinifAd)),
-          const Icon(Icons.class__outlined));
+      }, Observer(builder: (context) => Text(_viewModelSinif.sinifAd)), const Icon(Icons.class__outlined));
   _buildDersSec(BuildContext context) => myCustomMenuButton(context, () {
         showDialog(
                 context: context,
                 builder: (context) =>
-                    CustomDersDialog(onClickedDone: _addTransactionDers))
+                    CustomDersDialog(gelensinifId: _viewModelSinif.filtreSinifId, onClickedDone: _addTransactionDers))
             .then((value) {
+          _temrinSecimiSifirla();
           if (value != null) {
-            dersSecText = value.dersAd;
-            viewModelDers.setFiltreDersId(value.dersId);
+            _dersSecText = value.dersAd;
+            _viewModelDers.setFiltreDersId(value.dersId);
             if (value.dersId == -1) {
-              dersSecText = "Ders Seç";
+              _dersSecText = "Ders Seç";
             }
-            viewModelDers.setDersAd(dersSecText);
+            _viewModelDers.setDersAd(_dersSecText);
             Logger().i('Seçilen ders id ${value.dersId} ders: ${value.dersAd}');
           }
         });
-      }, Observer(builder: (context) => Text(viewModelDers.dersAd)),
-          const Icon(Icons.class__outlined));
+      }, Observer(builder: (context) => Text(_viewModelDers.dersAd)), const Icon(Icons.class__outlined));
+  _buildTemrinSec(BuildContext context) => myCustomMenuButton(context, () {
+        showDialog(
+                context: context,
+                builder: (context) =>
+                    CustomTemrinDialog(gelenDersId: _viewModelDers.filtredersId, onClickedDone: _addTransactionTemrin))
+            .then((value) {
+          if (value != null) {
+            _temrinSecText = value.temrinKonusu;
+            _viewModelTemrin.setFiltretemrinId(value.temrinId);
+            if (value.temrinId == -1) {
+              _temrinSecText = "Temrin Seç";
+            }
+            _viewModelTemrin.settemrinKonusu(_temrinSecText);
+            Logger().i('Seçilen temrin id ${value.temrinId} temrin konusu: ${value.temrinKonusu}');
+          }
+        });
+      }, Observer(builder: (context) => Text(_viewModelTemrin.temrinKonusu)), const Icon(Icons.class__outlined));
+
+  void _tumSecimleriSifirla() {
+    Logger().i('tum secimler sıfırlandı');
+    _dersSecimiSifirla();
+    _temrinSecimiSifirla();
+  }
+
+  void _dersSecimiSifirla() {
+    Logger().i('ders secimleri sıfırlandı');
+    _viewModelDers.setFiltreDersId(-1);
+    _dersSecText = "Ders Seç";
+    _viewModelDers.setDersAd(_dersSecText);
+  }
+
+  void _temrinSecimiSifirla() {
+    Logger().i('temrin secimleri sıfırlandı');
+    _viewModelTemrin.setFiltretemrinId(-1);
+    _temrinSecText = "Temrin Seç";
+    _viewModelTemrin.settemrinKonusu(_temrinSecText);
+  }
 }
