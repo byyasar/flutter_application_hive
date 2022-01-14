ab4fdd01216e948311e14698381e10d53130ff2a
yasar <byyasar@gmail.com>
2022-01-14 16:11:18 +0300

    yeni menu oluşturuldu

--- lib/view/mainpage.dart
+++ lib/view/mainpage.dart
@@ -1,7 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_application_hive/constants/app_constants.dart';
+import 'package:flutter_application_hive/core/base/base_state.dart';
+import 'package:flutter_application_hive/core/boxes.dart';
+import 'package:flutter_application_hive/core/widget/custom_ders_dialog.dart';
 import 'package:flutter_application_hive/core/widget/custom_menu_button.dart';
+import 'package:flutter_application_hive/core/widget/custom_sinif_dialog.dart';
 import 'package:flutter_application_hive/features/dersler/model/ders_model.dart';
+import 'package:flutter_application_hive/features/dersler/store/ders_store.dart';
 import 'package:flutter_application_hive/features/dersler/view/ders_view.dart';
 import 'package:flutter_application_hive/features/helper/ders_listesi_helper.dart';
 import 'package:flutter_application_hive/features/helper/ogrenci_listesi_helper.dart';
@@ -9,9 +14,11 @@ import 'package:flutter_application_hive/features/helper/sinif_listesi_helper.da
 import 'package:flutter_application_hive/features/ogrenci/model/ogrenci_model.dart';
 import 'package:flutter_application_hive/features/ogrenci/view/ogrenci_view.dart';
 import 'package:flutter_application_hive/features/siniflar/model/sinif_model.dart';
+import 'package:flutter_application_hive/features/siniflar/store/sinif_store.dart';
 import 'package:flutter_application_hive/features/siniflar/view/sinif_view.dart';
 import 'package:flutter_application_hive/features/temrin/view/temrin_view.dart';
 import 'package:flutter_application_hive/features/temrinnot/view/temrinnot_view.dart';
+import 'package:flutter_mobx/flutter_mobx.dart';
 import 'package:http/http.dart' as http;
 import 'dart:convert' as convert;
 import 'package:logger/logger.dart';
@@ -23,7 +30,19 @@ class MainPage extends StatefulWidget {
   State<MainPage> createState() => _MainPageState();
 }
 
-class _MainPageState extends State<MainPage> {
+class _MainPageState extends BaseState<MainPage> {
+  List<SinifModel> transactionsSinif = [];
+  List<DersModel> transactionsDers = [];
+
+  SinifStore viewModelSinif = SinifStore();
+  DersStore viewModelDers = DersStore();
+
+  String sinifSecText = "Sınıf Seç";
+  String dersSecText = "Ders Seç";
+
+  final DersListesiHelper _dersListesiHelper =
+      DersListesiHelper(ApplicationConstants.boxDers);
+
   @override
   Widget build(BuildContext context) {
     return SafeArea(
@@ -34,13 +53,10 @@ class _MainPageState extends State<MainPage> {
         body: Center(
           child: Column(
             children: [
-              const Text('Temrin Not Girişi'),
-              myCustomMenuButton(context, () {}, 'Sınıf Seçiniz',
-                  const Icon(Icons.class__outlined)),
-              myCustomMenuButton(context, () {}, 'Ders Seçiniz',
-                  const Icon(Icons.play_lesson)),
-              myCustomMenuButton(
-                  context, () {}, 'Temrin Seçiniz', const Icon(Icons.work)),
+              const Text('Sınıf:', style: TextStyle(fontSize: 18)),
+              _buildSinifSec(context),
+              const Text('Ders:', style: TextStyle(fontSize: 18)),
+              _buildDersSec(context),
             ],
           ),
         ),
@@ -214,4 +230,72 @@ class _MainPageState extends State<MainPage> {
       ),
     );
   }
+
+  _addTransactionSinif(int id, String sinifAd) async {
+    final transaction = SinifModel(id: id, sinifAd: sinifAd);
+    final box = SinifBoxes.getTransactions();
+    box.add(transaction);
+  }
+
+  _addTransactionDers(int id, String dersad, int sinifId) async {
+    final dersModel = DersModel(
+        id: id, dersad: dersad, sinifId: viewModelSinif.filtreSinifId);
+    final box = DersBoxes.getTransactions();
+    box.add(dersModel);
+  }
+
+  List<DersModel>? _getDersItemId(int id) {
+    List<DersModel> transactionsDersListesiGecici =
+        DersBoxes.getTransactions().values.toList().cast<DersModel>();
+    List<DersModel> transactionsDersListesiGeciciFiltre = [];
+
+    for (var ders in transactionsDersListesiGecici) {
+      if (ders.id == id) {
+        transactionsDersListesiGeciciFiltre.add(ders);
+      }
+    }
+    return transactionsDersListesiGeciciFiltre;
+  }
+
+  _buildSinifSec(BuildContext context) => myCustomMenuButton(context, () {
+        showDialog(
+                context: context,
+                builder: (context) =>
+                    CustomSinifDialog(onClickedDone: _addTransactionSinif))
+            .then((value) {
+          transactionsDers = [];
+          if (value != null) {
+            sinifSecText = value.sinifAd;
+            viewModelSinif.setFiltreSinifId(value.sinifId);
+
+            if (value.sinifId == -1) {
+              sinifSecText = "Sınıf Seç";
+            } else {
+              transactionsDers = _getDersItemId(viewModelSinif.filtreSinifId)!;
+            }
+            viewModelSinif.setSinifAd(sinifSecText);
+            Logger()
+                .i('Seçilen sınıf id ${value.sinifId} sınıf ${value.sinifAd}');
+          }
+        });
+      }, Observer(builder: (context) => Text(viewModelSinif.sinifAd)),
+          const Icon(Icons.class__outlined));
+  _buildDersSec(BuildContext context) => myCustomMenuButton(context, () {
+        showDialog(
+                context: context,
+                builder: (context) =>
+                    CustomDersDialog(onClickedDone: _addTransactionDers))
+            .then((value) {
+          if (value != null) {
+            dersSecText = value.dersAd;
+            viewModelDers.setFiltreDersId(value.dersId);
+            if (value.dersId == -1) {
+              dersSecText = "Ders Seç";
+            }
+            viewModelDers.setDersAd(dersSecText);
+            Logger().i('Seçilen ders id ${value.dersId} ders: ${value.dersAd}');
+          }
+        });
+      }, Observer(builder: (context) => Text(viewModelDers.dersAd)),
+          const Icon(Icons.class__outlined));
 }
